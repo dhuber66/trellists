db54595c50ac639c86687c16e39e25db3024cade
Vlad Savitsky <vlad.savitsky@gmail.com>
2014-11-26 21:06:20 +0200

    Version 1.5.

--- js/trellists.js
+++ js/trellists.js
@@ -1,20 +1,28 @@
+// TODO: when 'Points for Trello' changes header topBar updates and loose status of list. We should get this status from original List when rebuild topBar.
 // TODO: store state of Lists in LocalStorage.
 // TODO: add a 'Show all' button.
 
+// Trello.com loads lists after whole page loaded and DOM is ready so we need to
+// wait for some DOM-elements appear on page and then react on this change.
+// Custom jQuery plugin was used to wait while required DOM-element will be created.
+
 (function() {
 
-  // Insert bar placeholder to header.
-  // Custom jQuery plugin was used to wait while required DOM-element will be created.
-  $('.board-header').waitUntilExists(function() {
-    $('<ul/>').attr('id', 'trellists').appendTo('.board-header');
-  });
-  
-  // Trello.com loads lists after whole page loaded and DOM is ready so we need to 
-  // wait for some DOM-elements appear on page and then react on this change.
-
-  // Update Tabs on List insert/archive/remove.
+  // This element appears last at page and we use it to add the Menu to page and set status for each List.
   $('#board .list form .js-open-add-list').waitUntilExists(function() {
-    buildTabs();
+    if ($('#trellists').length == 0) {
+      // Insert bar placeholder to header. Should run only once.
+      $('<ul/>').attr('id', 'trellists').appendTo('.board-header');
+
+      // Set default 'show-list' status to all lists.
+      // TODO: store status in LocalStorage and save status of Lists there.
+      $('#board .list').each(function() {
+        $(this).removeClass('hide-list').addClass('show-list');
+      });
+    }
+
+    // Update Menu on List insert/archive/remove.
+    buildMenu();
   });
 
   // Update  list name on change. Already optimized.
@@ -24,7 +32,7 @@
       // Somehow List title could be empty and we need to pass by this case.
       // Compare old title and new one to run only on title change but not subtree changes or etc.
       if (name && name != $(this).parent().parent().attr('data-list-name')) {
-        buildTabs();
+        buildMenu();
       }
     });
   });
@@ -32,7 +40,7 @@
   // Update on List's drag-n-drop movements.
   $('#board .placeholder').waitUntilExists(function() {
     $('#board .placeholder').bind('DOMNodeRemovedFromDocument', function(e) {
-      buildTabs();
+      buildMenu();
     });
   });
 
@@ -45,28 +53,41 @@
    * Build Top Bar which shows titles of existing lists.
    * Each time new list created ate page we rebuild top bar.
    */
-  function buildTabs() {
+  function buildMenu() {
     var li = '';
     // Get all Lists at board except placeholder for new List creation to add them to the Bar.
-    $('#board .list').each(function () {
+    $('#board .list').each(function() {
       // Get only List's name without any sub-elements.
       var name = getListName($(this));
       if (name) {
-        // Create the tab for those List.
-        var tab = $('<li/>').addClass('show-list').attr('data-tab-name', name).text(name);
-        li += tab[0].outerHTML;
-        
+        // Create the tab in Menu for current List.
+        var tab = $('<li/>').attr('data-tab-name', name).text(name);
+
         // Mark a List to be able to find it later.
         // TODO: find a way to aviod this kind of marking and search by List's name.
-        $(this).addClass('show-list').removeClass('hide-list').attr("data-list-name", name);
+        if ($(this).attr("data-list-name") != name) {
+          // Update List name in attribute only if it's necessary.
+          $(this).attr("data-list-name", name);
+        }
+
+        // Tabs should duplicate status (hide/show) of actual List.
+        // Set default status 'show-list' and change it only if actual List has status.
+        if ($(this).hasClass('show-list')) {
+          $(tab).addClass('show-list');
+        }
+        else if ($(this).hasClass('hide-list')) {
+          $(tab).addClass('hide-list');
+        }
+        li += tab[0].outerHTML;
       }
     });
-    // Replace tabs in the TabBar.
-    $('#trellists').empty().append(li).show();
 
-    // Hides/shows List on click at tab.    
+    // Replace tabs in the Menu.
+    $('#trellists').empty().append(li);
+
+    // Hides/shows List on click at tab.
     // We need to attach onClick behaviour for newly created tabs just after they was added to DOM.
-    $('#trellists li').click(function () {
+    $('#trellists li').click(function() {
       var $list = $("#board .list[data-list-name='" + $(this).attr('data-tab-name') +"']");
       //TODO: use jQuery .toggle instead code below.
       if ($list.hasClass('show-list')) {
