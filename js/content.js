520a5e60ccfc6609936180e04a5b2f980423f278
Vlad Savitsky <vlad.savitsky@gmail.com>
2014-11-26 17:18:15 +0200

    Version 1.0.

---
+++ js/content.js
@@ -0,0 +1,50 @@
+(function(window){
+  /**
+   * Initialize Top Bar.
+   */ 
+  function topBarTrelloInit() {
+    var HTMLmarkup = '<div class="top-bar"><ul>';
+    var hidden_list_id = 0;
+    // Get all lists on board:
+	  $('.list-area .list').each(function () {
+      // Mark necessary elements:
+      $(this).addClass('show-list').removeClass('hide-list').append('<div class="list-id" id="' + hidden_list_id + '"></div>');    
+      // Create HTML list:
+      HTMLmarkup += '<li><span class="top-bar-tab show-list"><span class="app-icon small-icon list-icon"></span><span ' +
+        'class="name">' + $(this).find('.list-title h2').text() + '</span><div ' + 
+        'class="list-id" id="' + hidden_list_id + '"></div></span></li>';
+      hidden_list_id++;
+	  });
+    HTMLmarkup += '</ul></div>';
+    // Insert Top Bar into DOM:
+    $('.top-bar').remove();
+    $('.board-title').append(HTMLmarkup);
+
+    // Top Bar click reaction:
+	  $('.top-bar-tab').click(function () {
+      toggleList($(this));
+    });
+  }
+
+  /**
+   * Decide should we show or hide List:
+   */
+  function toggleList($element) {
+    var id = $element.find('.list-id').attr('id');
+    var $list = $('.list-area .list').find('#' +id).parent('.list');
+    var $tab = $('.top-bar-tab').find('#' +id).parents('.top-bar-tab');
+    if ($list.hasClass('show-list')) {
+      // Hide
+      $list.addClass('hide-list').removeClass('show-list').hide();
+      $tab.addClass('hide-list').removeClass('show-list');
+    } else {
+      // Show
+      $list.addClass('show-list').removeClass('hide-list').show();
+      $tab.addClass('show-list').removeClass('hide-list');
+    }
+  }
+  // Attach initialization to onLoad event.
+  if(window.addEventListener) {
+	  window.addEventListener("load", function() { topBarTrelloInit(); } ,false);
+  }
+}) (window);
